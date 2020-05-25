ed98f2c8701d4cce408a558f7efb98db9d9436eb
RistainoGiuseppe <giuseppe.ristaino@gmail.com>
2020-05-25 15:44:00 +0200

    Add Navbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {Switch, Route} from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import Default from "./components/Default";
+import Details from "./components/Details";
+import Cart from "./components/Cart";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+
+        <React.Fragment>
+          <h3>Hello from app</h3>
+
+          <Navbar></Navbar>
+            <Switch>
+                <Route exact path={"/"} component={ProductList}/>
+                <Route path={"/details"} component={Details}/>
+                <Route path={"/cart"} component={Cart}/>
+                <Route component={Default}/>
+            </Switch>
+
+        </React.Fragment>
+
+        /* Sostituiamo con REACT
+        <div className="container">
+          <div className="row">
+            <div className="col-6">column number one</div>
+            <div className="col-6">
+              <span>
+                <i className="fas fa-home"/>
+              </span>
+            </div>
+          </div>
+        </div>
+        */
+
+        /*
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo"/>
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+         */
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import logo from '../logo.png'
+
+
+class Navbar extends Component {
+    render() {
+        return (
+            <nav className={"navbar navbar-expand-sm bg-primary navbar-dark px-sm-5"}>
+                {/*
+                https://fontawesome.com/kits/a27ebff0c6/settings
+                */}
+                <Link to={'/'}>
+                    <img src={logo} alt={"store"}
+                         className={"navbar-brand"}/>
+                </Link>
+                <ul className={"navbar-nav align-items-center"}>
+                    <li className={"nav-item ml-5"}>
+                        <Link to={"/"} className={"nav-link"}>
+                        products
+                        </Link>
+                    </li>
+                </ul>
+
+                <Link to={"/cart"} className={"ml-auto"}>
+                    <button>
+                        <i className={"fas fa-cart-plus"}/>
+                        My Cart
+                    </button>
+                </Link>
+            </nav>
+        );
+    }
+}
+
+export default Navbar;
